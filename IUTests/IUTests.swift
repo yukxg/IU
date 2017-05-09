325399c8cc23ce6bb969dbe18f262ba9ef84c8c4
ZHIXUAN YU <yu343@purdue.edu>
2017-05-09 18:20:11 -0400

    Initial Commit

---
+++ IUTests/IUTests.swift
@@ -0,0 +1,36 @@
+//
+//  IUTests.swift
+//  IUTests
+//
+//  Created by ZHIXUAN YU on 5/9/17.
+//  Copyright © 2017 ZHIXUAN YU. All rights reserved.
+//
+
+import XCTest
+@testable import IU
+
+class IUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
